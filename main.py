4df8cce0f4d5823631c1ad9418b4bf8f64846215
camarada_nikc <nicole.p.goncalves@aluno.senai.br>
2026-02-11 14:19:51 -0300

    flet ola mundo

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import flet as ft 
 
 def main(page: ft.Page):
+    page.title = "Meu Primeiro app Flet"
+    page.bgcolor = "#8d10e6"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER    
     page.add(
         ft.Text(value="Nicole!!!"),
         ft.ElevatedButton("Clique aqui") 
